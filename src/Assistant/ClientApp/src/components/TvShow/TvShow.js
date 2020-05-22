aa8757a373f244c40666e87779cf72ad7c180561
AnthonyScottMoncrieff <anthony.scottmoncrieff@gmail.com>
2020-05-22 17:42:09 +0100

    Removed unused FetchData

--- src/Assistant/ClientApp/src/components/TvShow/TvShow.js
+++ src/Assistant/ClientApp/src/components/TvShow/TvShow.js
@@ -5,7 +5,7 @@ const tvShow = (props) => (
     <div className={classes.TvShow}>
         <img src={props.thumbnailUrl} alt="Thumbnail" />
         <div className={classes.ShowName}>{props.showName}</div>
-        <div className="show-description">{props.description}</div>
+        <div className="show-description">{props.description.length > 322 ? `${props.description.substring(0, 322)}...` : props.description }</div>
     </div>
 )
 
