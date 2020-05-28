9d6cef58b25812a00c748a680d52649f1270eb59
AnthonyScottMoncrieff <anthony.scottmoncrieff@gmail.com>
2020-05-28 22:10:09 +0100

    Small Add CSS modification

--- src/Assistant/ClientApp/src/components/UI/Add/Add.js
+++ src/Assistant/ClientApp/src/components/UI/Add/Add.js
@@ -2,10 +2,13 @@ import React from 'react';
 import classes from './Add.module.css';
 
 const add = (props) => (
-    <div className={classes.AddContainer}>
-        <div className={classes.Add} onClick={props.clicked}>+</div>
-        <div className={classes.Label}>Add a new TV Show</div>
+    <div className={classes.AddRoot}>
+        <div className={classes.AddContainer}>
+            <div className={classes.Add} onClick={props.clicked}>+</div>
+            <div className={classes.Label}>Add a new TV Show</div>
+        </div>
     </div>
+
 );
 
 export default add;
