659a700208ee4e2349efff13a6549caaa3744e0e
AnthonyScottMoncrieff <anthony.scottmoncrieff@gmail.com>
2020-07-20 16:47:17 +0100

    Added testStore to utilities

--- src/Assistant/ClientApp/src/TestAssets/utilities.js
+++ src/Assistant/ClientApp/src/TestAssets/utilities.js
@@ -1,4 +1,18 @@
 import checkPropTypes from 'check-prop-types';
+import createSagaMiddleware from "redux-saga";
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import tvShows from '../store/reducers/tvShows';
+import episodes from '../store/reducers/episodes';
+
+export const testStore = (initialState) => {
+    const rootReducer = combineReducers({
+        tvShows: tvShows,
+        episodes: episodes
+    });
+    const sagaMiddleware = createSagaMiddleware();
+    const createStoreWithMiddleware = applyMiddleware([sagaMiddleware])(createStore);
+    return createStoreWithMiddleware(rootReducer, initialState);
+}
 
 export const findByTestAtrr = (component, attr) => {
     const wrapper = component.find(`[data-test='${attr}']`);
