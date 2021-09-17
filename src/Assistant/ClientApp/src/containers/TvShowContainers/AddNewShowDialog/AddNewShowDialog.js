739abdbf87b3ed38c275a9b32addd2f5341f164e
Anthony Moncrieff <Anthony.Moncrieff@stepstone.com>
2021-09-17 11:45:56 +0100

    Started migrating to hooks

--- src/Assistant/ClientApp/src/containers/TvShowContainers/AddNewShowDialog/AddNewShowDialog.js
+++ src/Assistant/ClientApp/src/containers/TvShowContainers/AddNewShowDialog/AddNewShowDialog.js
@@ -6,7 +6,7 @@ import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import ShowPreview from '../../../components/TvShowComponents/ShowPreview/ShowPreview';
 import axios from 'axios';
-import * as actions from '../../../store/actions'
+import * as actions from '../../../store/actions';
 import { connect } from 'react-redux';
 import Error from '../../../components/UI/Error/Error';
 import PropTypes from 'prop-types';
@@ -17,91 +17,121 @@ class AddNewShowDialog extends Component {
             userDefinedName: {
                 elementConfig: {
                     type: 'text',
-                    placeholder: 'Show Name'
+                    placeholder: 'Show Name',
                 },
                 value: '',
                 validation: {
-                    expression: /^.+$/g
+                    expression: /^.+$/g,
                 },
                 valid: false,
-                touched: false
+                touched: false,
             },
             selectedShow: null,
             fetchingShow: false,
             error: false,
-            errorMessage: "",
-            searchTerm: ""
-        }
-    }
+            errorMessage: '',
+            searchTerm: '',
+        },
+    };
 
     submitClickHandler = () => {
-        if (this.props.shows.filter(x => x.showKey === this.state.tvShowRequest.searchTerm).length === 0) {
-            let updatedTvshowRequest = updateObject(this.state.tvShowRequest, { error: false, errorMessage: "" });
+        if (this.props.shows.filter((x) => x.showKey === this.state.tvShowRequest.searchTerm).length === 0) {
+            let updatedTvshowRequest = updateObject(this.state.tvShowRequest, {
+                error: false,
+                errorMessage: '',
+            });
             this.setState({ tvShowRequest: updatedTvshowRequest });
-            this.props.onSubmitTvShow(this.state.tvShowRequest.selectedShow, this.state.tvShowRequest.searchTerm, () => this.cancelSubmissionHandler());
-        }
-        else {
-            let updatedTvshowRequest = updateObject(this.state.tvShowRequest, { error: true, errorMessage: "ERROR: You attempted to add a duplicate show" });
+            this.props.onSubmitTvShow(this.state.tvShowRequest.selectedShow, this.state.tvShowRequest.searchTerm, () =>
+                this.cancelSubmissionHandler()
+            );
+        } else {
+            let updatedTvshowRequest = updateObject(this.state.tvShowRequest, {
+                error: true,
+                errorMessage: 'ERROR: You attempted to add a duplicate show',
+            });
             this.setState({ tvShowRequest: updatedTvshowRequest });
         }
-    }
+    };
 
     cancelSubmissionHandler = () => {
-        this.updateUserDefinedName({ value: "", valid: false })
-        let updatedTvshowRequest = updateObject(this.state.tvShowRequest, { selectedShow: null, searchTerm: "" });
+        this.updateUserDefinedName({ value: '', valid: false });
+        let updatedTvshowRequest = updateObject(this.state.tvShowRequest, {
+            selectedShow: null,
+            searchTerm: '',
+        });
         this.setState({ tvShowRequest: updatedTvshowRequest });
         document.getElementById('modal-backdrop').click();
-    }
+    };
 
     fetchShowHandler = () => {
         let showRequest = this.state.tvShowRequest;
-        let parsedValue = showRequest.userDefinedName.value.replace(/(?!\s)[\W]/g, "").replace(/\s/g, "-");
-        this.setState({ tvShowRequest: updateObject(this.state.tvShowRequest, { fetchingShow: true, searchTerm: parsedValue }) });
-        axios.get(`https://api.tvmaze.com/singlesearch/shows?q=${parsedValue}`)
+        let parsedValue = showRequest.userDefinedName.value.replace(/(?!\s)[\W]/g, '').replace(/\s/g, '%20');
+        this.setState({
+            tvShowRequest: updateObject(this.state.tvShowRequest, {
+                fetchingShow: true,
+                searchTerm: parsedValue,
+            }),
+        });
+        axios
+            .get(`https://api.tvmaze.com/singlesearch/shows?q=${parsedValue}`)
             .then((response) => {
-                let updatedTvshowRequest = updateObject(this.state.tvShowRequest, { selectedShow: response.data, fetchingShow: false, error: false, errorMessage: "" });
+                let updatedTvshowRequest = updateObject(this.state.tvShowRequest, {
+                    selectedShow: response.data,
+                    fetchingShow: false,
+                    error: false,
+                    errorMessage: '',
+                });
                 this.setState({ tvShowRequest: updatedTvshowRequest });
             })
-            .catch(_ => {
-                let updatedTvshowRequest = updateObject(this.state.tvShowRequest, { fetchingShow: false, error: true, errorMessage: "ERROR: Cannot find show with this name" });
+            .catch((_) => {
+                let updatedTvshowRequest = updateObject(this.state.tvShowRequest, {
+                    fetchingShow: false,
+                    error: true,
+                    errorMessage: 'ERROR: Cannot find show with this name',
+                });
                 this.setState({ tvShowRequest: updatedTvshowRequest });
-            })
-    }
+            });
+    };
 
     updateUserDefinedName = (prop) => {
         let userDefinedName = updateObject(this.state.tvShowRequest.userDefinedName, prop);
         this.setState({
             tvShowRequest: updateObject(this.state.tvShowRequest, {
-                userDefinedName: userDefinedName
-            })
-        })
-    }
+                userDefinedName: userDefinedName,
+            }),
+        });
+    };
 
     userDefinedNameChangedHandler = (event) => {
-        if (!this.state.tvShowRequest.userDefinedName.touched)
-            this.updateUserDefinedName({ touched: true });
+        if (!this.state.tvShowRequest.userDefinedName.touched) this.updateUserDefinedName({ touched: true });
 
         let isValid = event.target.value.match(this.state.tvShowRequest.userDefinedName.validation.expression) !== null;
         this.updateUserDefinedName({ valid: isValid, value: event.target.value });
-    }
+    };
 
     render() {
         let showRequest = this.state.tvShowRequest;
-        let selectedShowDisplay = showRequest.error || (showRequest.selectedShow === null && !showRequest.fetchingShow)
-            ? null
-            : showRequest.selectedShow === null && showRequest.fetchingShow
-                ? <Spinner>Loading...</Spinner>
-                : 
+        let selectedShowDisplay =
+            showRequest.error || (showRequest.selectedShow === null && !showRequest.fetchingShow) ? null : showRequest.selectedShow === null &&
+              showRequest.fetchingShow ? (
+                <Spinner>Loading...</Spinner>
+            ) : (
                 <Fragment>
                     <ShowPreview
-                    showImg={toHttps(showRequest.selectedShow.image.medium)}
-                    showName={showRequest.selectedShow.name}
-                    showSummary={showRequest.selectedShow.summary} />
+                        showImg={toHttps(showRequest.selectedShow.image.medium)}
+                        showName={showRequest.selectedShow.name}
+                        showSummary={showRequest.selectedShow.summary}
+                    />
                     <div className={classes.SubmitRegion}>
-                        <Button btnType="Success" clicked={this.submitClickHandler} disabled={this.props.loading}>Submit</Button>
-                        <Button btnType="Danger" clicked={this.cancelSubmissionHandler}>Cancel</Button>
+                        <Button btnType="Success" clicked={this.submitClickHandler} disabled={this.props.loading}>
+                            Submit
+                        </Button>
+                        <Button btnType="Danger" clicked={this.cancelSubmissionHandler}>
+                            Cancel
+                        </Button>
                     </div>
-                </Fragment>;
+                </Fragment>
+            );
 
         return (
             <div className={classes.AddNewShowDialog}>
@@ -114,38 +144,43 @@ class AddNewShowDialog extends Component {
                             value={showRequest.userDefinedName.value}
                             invalid={!showRequest.userDefinedName.valid}
                             touched={showRequest.userDefinedName.touched}
-                            changed={this.userDefinedNameChangedHandler} />
+                            changed={this.userDefinedNameChangedHandler}
+                        />
                     </div>
                     <div className={classes.TvShowEntrySearchButton}>
-                        <Button btnType="Success" disabled={!showRequest.userDefinedName.valid} clicked={this.fetchShowHandler}>Search</Button>
+                        <Button btnType="Success" disabled={!showRequest.userDefinedName.valid} clicked={this.fetchShowHandler}>
+                            Search
+                        </Button>
                     </div>
                 </div>
                 {selectedShowDisplay}
                 <Error isVisible={this.state.tvShowRequest.error}>{this.state.tvShowRequest.errorMessage}</Error>
             </div>
-        )
+        );
     }
 }
 
 AddNewShowDialog.propTypes = {
     loading: PropTypes.bool,
     onSubmitTvShow: PropTypes.func,
-    shows: PropTypes.arrayOf(PropTypes.shape({
-        showKey: PropTypes.string
-    }))
-}
+    shows: PropTypes.arrayOf(
+        PropTypes.shape({
+            showKey: PropTypes.string,
+        })
+    ),
+};
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
     return {
         loading: state.tvShows.tvShowSubmissionLoading,
         error: state.tvShows.tvShowSubmissionError,
-        shows: state.tvShows.shows
+        shows: state.tvShows.shows,
     };
 };
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
     return {
-        onSubmitTvShow: (show, key, closeDialog) => show === null ? null : dispatch(actions.initTvShowSubmission(show, key, closeDialog))
+        onSubmitTvShow: (show, key, closeDialog) => (show === null ? null : dispatch(actions.initTvShowSubmission(show, key, closeDialog))),
     };
 };
 
--- src/Assistant/ClientApp/src/containers/TvShowContainers/EpisodeGrouping/EpisodeGrouping.js
+++ src/Assistant/ClientApp/src/containers/TvShowContainers/EpisodeGrouping/EpisodeGrouping.js
@@ -1,47 +1,41 @@
-import React, { Component, Fragment } from 'react';
+import React, { useState, Fragment } from 'react';
 import classes from './EpisodeGrouping.module.css';
 import Episode from '../../../components/TvShowComponents/Episode/Episode';
 import EpisodeGroupingHeader from '../../../components/TvShowComponents/EpisodeGroupingHeader/EpisodeGroupingHeader';
 import PropTypes from 'prop-types';
 
-class EpisodeGrouping extends Component {
-    state = {
-        isCollapsed: true
-    }
+const EpisodeGrouping = (props) => {
+    const [isCollapsed, setIsCollapsed] = useState(true);
+    const toggleClass = isCollapsed ? [classes.EpisodeCollection, classes.Collapsed].join(' ') : classes.EpisodeCollection;
 
-    toggleCollapsed = () => {
-        this.setState({ isCollapsed: !this.state.isCollapsed })
-    }
-
-    render() {
-        let toggleClass = this.state.isCollapsed ? [classes.EpisodeCollection, classes.Collapsed].join(" ") : classes.EpisodeCollection;
-        return (
-            <div className={classes.EpisodeGrouping}>
-                <div className={classes.Title} onClick={this.toggleCollapsed}>{`Season ${this.props.grouping.key}`}</div>
-                <div className={toggleClass}>
-                    <EpisodeGroupingHeader />
-                    {this.props.grouping.groupedCollection
-                        .sort((a, b) => b.number - a.number)
-                        .map(e =>
-                            <Fragment key={e.id}>
-                                <Episode episode={e} shouldLoadImg={!this.state.isCollapsed} />
-                                <div className={classes.Divider}></div>
-                            </Fragment>
-                        )}
-                </div>
+    return (
+        <div className={classes.EpisodeGrouping}>
+            <div className={classes.Title} onClick={() => setIsCollapsed(!isCollapsed)}>{`Season ${props.grouping.key}`}</div>
+            <div className={toggleClass}>
+                <EpisodeGroupingHeader />
+                {props.grouping.groupedCollection
+                    .sort((a, b) => b.number - a.number)
+                    .map((e) => (
+                        <Fragment key={e.id}>
+                            <Episode episode={e} shouldLoadImg={!isCollapsed} />
+                            <div className={classes.Divider}></div>
+                        </Fragment>
+                    ))}
             </div>
-        )
-    }
+        </div>
+    );
 };
 
 EpisodeGrouping.propTypes = {
     grouping: PropTypes.shape({
         key: PropTypes.number,
-        groupedCollection: PropTypes.arrayOf(PropTypes.shape({
-            number: PropTypes.number,
-            id: PropTypes.number
-        }))
-    })
-}
+        groupedCollection: PropTypes.arrayOf(
+            PropTypes.shape({
+                number: PropTypes.number,
+                id: PropTypes.number,
+            })
+        ),
+    }),
+};
 
 export default EpisodeGrouping;
